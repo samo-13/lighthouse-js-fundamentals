218980de75b256b187b75c0cbbce17b4655d94e0
Sarah Moss <sarah-moss@live.com>
2022-01-23 23:41:23 +0000

    is odd

--- lighthouse/lighthouse-js-fundamentals/conditionals.js
+++ lighthouse/lighthouse-js-fundamentals/conditionals.js
@@ -50,3 +50,21 @@ if (!raining) {
   console.log("Leave your umbrella at home!");
 }
 
+// wichSchool Assignment
+const whichSchool  = function (age) {
+  if (age < 13) {
+    console.log("Elemenatary School");
+  }
+  else if (age >= 13 || <=18) {
+    console.log("Secondary School");
+  }
+  else {
+    console.log("Lighthouse Labs");
+  }
+}
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
